49b4681ad1f3d85ac0597a4a30a7fd6969581e94
devanshg5609 <devanshg5609@gmail.com>
2026-01-14 13:33:50 +0000

    feat: update files

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Simple service worker for PWA
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+  });
